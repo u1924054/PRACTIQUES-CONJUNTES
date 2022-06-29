f4c01108b5d27fc3216af066e509e8a037db9214
Dalila <u1924054@campus.udg.edu>
2022-06-29 14:29:13 +0200

    merge

---
+++ endevinalles.py
@@ -0,0 +1,13 @@
+
+#generar un numero aleatori de l'1 al 9
+
+import random
+
+a= random.randint(1,9)
+
+numero = int(input("Introdueix un numero aleatori de 1 a 9 tots dos inclosos: "))
+
+if a== numero:
+    print('Ho has adivinat.')
+else:
+    print ('No ho has adivinat')
